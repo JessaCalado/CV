f3e4a4cbe9339d5c02df1b460b39935504e0ca4e
JessaCalado <121009631+JessaCalado@users.noreply.github.com>
2022-12-19 00:04:56 -0800

    commit

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+
+import 'HomePage.dart';
+
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    initialRoute: '/',
+    routes: {
+      '/': (context) => const HomeRoute(),
+
+
+    },
+  )); //MaterialApp
+}
+
+class HomeRoute extends StatelessWidget {
+  const HomeRoute({key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Cocktail App',
+      // theme: ThemeData(
+      //   primarySwatch: Colors.blue,
+      // ),
+      home: HomePage(),
+    );
+  }
+}
+
